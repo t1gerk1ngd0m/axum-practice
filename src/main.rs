23446eab56bb904abfe24360c5642875bb065b94
Taiga Miwa <taiga.miwa888@gmail.com>
2023-11-03 22:30:27 +0900

    postできるようにした

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,41 @@
-use axum::{http::StatusCode, routing::get, Router};
+use axum::{http::StatusCode, response, routing::*, Json, Router, Server};
+use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
-// use serde::{Deserialize, Serialize};
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
 
-    let app = Router::new().route("/", get(root));
-
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/ping", post(ping));
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::debug!("Listening on {}", addr);
-    axum::Server::bind(&addr)
+    Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
 }
 
+#[derive(Deserialize)]
+struct Ping {
+    count: i64,
+}
+
+#[derive(Serialize)]
+struct Pong {
+    count: i64,
+}
+
 async fn root() -> (StatusCode, &'static str) {
     (StatusCode::OK, "Hello, World!")
+}
+
+async fn ping(Json(ping): Json<Ping>) -> (StatusCode, Json<Pong>) {
+    (
+        StatusCode::ACCEPTED,
+        response::Json(Pong {
+            count: ping.count + 1,
+        }),
+    )
 }

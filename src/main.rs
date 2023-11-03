352992ff4f310e2f62888ad0856482088ac1788d
Taiga Miwa <taiga.miwa888@gmail.com>
2023-11-04 08:44:36 +0900

    tab-size変更

--- src/main.rs
+++ src/main.rs
@@ -4,38 +4,38 @@ use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::fmt::init();
+  tracing_subscriber::fmt::init();
 
-    let app = Router::new()
-        .route("/", get(root))
-        .route("/ping", post(ping));
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    tracing::debug!("Listening on {}", addr);
-    Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+  let app = Router::new()
+    .route("/", get(root))
+    .route("/ping", post(ping));
+  let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+  tracing::debug!("Listening on {}", addr);
+  Server::bind(&addr)
+    .serve(app.into_make_service())
+    .await
+    .unwrap();
 }
 
 #[derive(Deserialize)]
 struct Ping {
-    count: i64,
+  count: i64,
 }
 
 #[derive(Serialize)]
 struct Pong {
-    count: i64,
+  count: i64,
 }
 
 async fn root() -> (StatusCode, &'static str) {
-    (StatusCode::OK, "Hello, World!")
+  (StatusCode::OK, "Hello, World!")
 }
 
 async fn ping(Json(ping): Json<Ping>) -> (StatusCode, Json<Pong>) {
-    (
-        StatusCode::ACCEPTED,
-        response::Json(Pong {
-            count: ping.count + 1,
-        }),
-    )
+  (
+    StatusCode::ACCEPTED,
+    response::Json(Pong {
+      count: ping.count + 1,
+    }),
+  )
 }

ce94349f0e2491dffc88a83792f2f75ab9f99f52
Taiga Miwa <taiga.miwa888@gmail.com>
2023-11-04 23:04:30 +0900

    workspace導入&get_todos実装

---
+++ crates/rest/src/main.rs
@@ -0,0 +1,87 @@
+use axum::{
+  body::Body,
+  http::StatusCode,
+  response,
+  response::{IntoResponse, Response},
+  routing::*,
+  Json, Router, Server,
+};
+use serde::{Deserialize, Serialize};
+use std::net::SocketAddr;
+use uuid::Uuid;
+
+#[tokio::main]
+async fn main() {
+  tracing_subscriber::fmt::init();
+
+  let app = Router::new()
+    .route("/", get(root))
+    .route("/ping", post(ping))
+    .route("/todos", get(get_todos));
+  // .route("/todos", post(create_todo))
+  // .route("/todos/:id", get(get_todo))
+  // .route("/todos/:id", put(update_todo))
+  // .route("/todos/:id", delete(delete_todo));
+  let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+  tracing::debug!("Listening on {}", addr);
+  Server::bind(&addr)
+    .serve(app.into_make_service())
+    .await
+    .unwrap();
+}
+
+#[derive(Deserialize)]
+struct Ping {
+  count: i64,
+}
+
+#[derive(Serialize)]
+struct Pong {
+  count: i64,
+}
+
+async fn root() -> (StatusCode, &'static str) {
+  (StatusCode::OK, "Hello, World!")
+}
+
+async fn ping(Json(ping): Json<Ping>) -> (StatusCode, Json<Pong>) {
+  (
+    StatusCode::ACCEPTED,
+    response::Json(Pong {
+      count: ping.count + 1,
+    }),
+  )
+}
+
+async fn get_todos() -> impl IntoResponse {
+  let todos = TodosResponse {
+    todos: vec![
+      TodoResponse {
+        id: Uuid::new_v4(),
+        text: "test1".to_string(),
+        cmpleted: true,
+      },
+      TodoResponse {
+        id: Uuid::new_v4(),
+        text: "test2".to_string(),
+        cmpleted: false,
+      },
+    ],
+  };
+  Response::builder()
+    .status(StatusCode::OK)
+    .body(Body::from(serde_json::to_string(&todos).unwrap()))
+    .unwrap()
+}
+
+#[derive(Serialize)]
+struct TodoResponse {
+  id: Uuid,
+  text: String,
+  cmpleted: bool,
+}
+
+#[derive(Serialize)]
+struct TodosResponse {
+  todos: Vec<TodoResponse>,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
-use axum::{http::StatusCode, response, routing::*, Json, Router, Server};
+use axum::{
+  extract::{Query, State},
+  http::StatusCode,
+  response,
+  response::IntoResponse,
+  routing::*,
+  Json, Router, Server,
+};
 use serde::{Deserialize, Serialize};
-use std::net::SocketAddr;
+use std::{
+  collections::HashMap,
+  net::SocketAddr,
+  sync::{Arc, RwLock},
+};
+use uuid::Uuid;
 
 #[tokio::main]
 async fn main() {
@@ -9,6 +21,7 @@ async fn main() {
   let app = Router::new()
     .route("/", get(root))
     .route("/ping", post(ping));
+  // .route("todos", get(todos_index));
   let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
   tracing::debug!("Listening on {}", addr);
   Server::bind(&addr)
@@ -27,6 +40,12 @@ struct Pong {
   count: i64,
 }
 
+#[derive(Deserialize)]
+struct Pagination {
+  limit: Option<u32>,
+  offset: Option<u32>,
+}
+
 async fn root() -> (StatusCode, &'static str) {
   (StatusCode::OK, "Hello, World!")
 }
@@ -38,4 +57,19 @@ async fn ping(Json(ping): Json<Ping>) -> (StatusCode, Json<Pong>) {
       count: ping.count + 1,
     }),
   )
+}
+
+async fn todos_index(
+  pagination: Option<Query<Pagination>>,
+  State(db): State<Db>,
+) -> impl IntoResponse {
+}
+
+type Db = Arc<RwLock<HashMap<Uuid, Todo>>>;
+
+#[derive(Serialize)]
+struct Todo {
+  id: Uuid,
+  text: String,
+  cmpleted: bool,
 }
